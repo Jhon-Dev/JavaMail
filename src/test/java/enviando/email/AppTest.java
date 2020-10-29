52039179ef8c00712003360d9e576ee1d024a66c
Jhonatan Nuss <jhonatan.nuss082897@gmail.com>
2020-10-29 08:35:44 -0300

    Criando o projeto JavaMail com Maven

---
+++ src/test/java/enviando/email/AppTest.java
@@ -0,0 +1,14 @@
+package enviando.email;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+	 @org.junit.Test
+	public void testeEmail() {
+		
+		/*Olhe as configurações smtl do seu email*/
+		 
+	}
+}
