cdb5076c7f830fdc5a4b2aaae01a8ff272c2e429
Jhonatan Nuss <jhonatan.nuss082897@gmail.com>
2020-10-29 23:53:14 -0300

    Configurando conexão com servidor Gmail

--- src/test/java/enviando/email/AppTest.java
+++ src/test/java/enviando/email/AppTest.java
@@ -2,14 +2,22 @@ package enviando.email;
 
 import java.util.Properties;
 
+import javax.mail.Authenticator;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+
 /**
  * Unit test for simple App.
  */
 public class AppTest {
+	
+	private String userName = "jhonatanemailformacaojavaweb@gmail.com";
+	private String senha = "JH.08.28.97";
 
 	 @org.junit.Test
 	public void testeEmail() {
-		
+		try {
+		 
 		/*Olhe as configurações smtl do seu email*/
 		 
 		 Properties properties = new Properties();
@@ -19,5 +27,19 @@ public class AppTest {
 		 properties.put("mail.smtp.port", "465");  /*Porta do servidor*/
 		 properties.put("mail.smtp.socketFactory.port", "465");  /*Expecifica a porta a ser conectada pelo socket*/
 		 properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");  /*Class socket de conexão ao SMTP*/
+		
+		Session session = Session.getInstance(properties, new  Authenticator() {
+		
+			@Override
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(userName, senha);
+			}
+		});
+		
+		System.out.println(session);
+		
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
